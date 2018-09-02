80f8ad556ef8ba9ba58fb9f26a9b848496fa2da3
tani14 <>
2018-09-02 18:31:25 +0900

    atomフィード取得して一覧表示した

--- jma/urls.py
+++ jma/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'jma'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('feedList', views.feedList, name='feedList'),
+    path('analysis', views.analysis, name='analysis'),
 ]
--- jma/views.py
+++ jma/views.py
@@ -1,10 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+import feedparser
 import logging
 logger = logging.getLogger(__name__)
+
+# 気象庁Atomフィード（地震火山）
+URL_EQVOL = 'https://www.data.jma.go.jp/developer/xml/feed/eqvol.xml'
 
 
 def index(request):
     logger.info('*** exec viwes.index ***')
     context = {'hoge': 'hoge'}
     return render(request, 'jma/index.html', context)
+
+
+def feedList(request):
+    logger.info('*** exec viwes.feedList ***')
+    entries = feedparser.parse(URL_EQVOL)['entries']
+    # logger.debug(entries)
+    context = {'entries': entries}
+    return render(request, 'jma/feedList.html', context)
+
+
+def analysis(request):
+    logger.info('*** exec viwes.analysis ***')
+    logger.debug(request)
+    # id = request.GET.get(key="id", default="none")
+
+    if "id" in request.GET:
+        # idが指定されている場合の処理
+        # ここで解析関数を呼ぶ
+        return HttpResponse("call = " + request.GET.get(key="id", default="none"))
+    else:
+        # idが指定されていない場合の処理
+        return HttpResponse("error!!")
