d492e6e9b530f3ba9532b2283707a416955ae267
tani14 <>
2018-09-16 16:26:22 +0900

    maxlength指定した

--- jma/models.py
+++ jma/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # AreaInformationCity.
 class AreaInformationCity(models.Model):
     code = models.CharField(verbose_name='市区町村コード', max_length=7, )
-    name = models.CharField(verbose_name='市区町村名')
+    name = models.CharField(verbose_name='市区町村名', max_length=20, )
     lon = models.CharField(verbose_name='経度', max_length=10, null=True, blank=True, )
     lat = models.CharField(verbose_name='緯度', max_length=10, null=True, blank=True, )
 
