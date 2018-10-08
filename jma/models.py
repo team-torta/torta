340d3838b7dd58dd5528282b3ec86bac69abb13c
ena <>
2018-10-08 18:10:30 +0900

    コメントつけたり、カラム名変えたり

--- jma/models.py
+++ jma/models.py
@@ -11,18 +11,20 @@ class AreaInformationCity(models.Model):
     def __str__(self):
         return self.code + ":" + self.name
 
+
 class Earthquake(models.Model):
     id = models.AutoField(primary_key=True)
     uuid = models.CharField(verbose_name='UUID', max_length=40, )
     date = models.DateTimeField(verbose_name='発生日時')
     maxint = models.CharField(verbose_name='最大震度', max_length=1,  null=True, blank=True, )
     epicenter = models.CharField(verbose_name='震源地', max_length=10,  null=True, blank=True, )
-    lon = models.CharField(verbose_name='経度', max_length=7,  null=True, blank=True, )
-    lat = models.CharField(verbose_name='緯度', max_length=7,  null=True, blank=True, )
+    center_lon = models.CharField(verbose_name='震源地経度', max_length=7,  null=True, blank=True, )
+    center_lat = models.CharField(verbose_name='震源地緯度', max_length=7,  null=True, blank=True, )
     upddt = models.DateTimeField(verbose_name='更新日時',  null=True, blank=True, )
 
     def __str__(self):
         return self.uuid
+
 
 class EarthquakeDetail(models.Model):
     uuid = models.ForeignKey(Earthquake, on_delete=models.CASCADE)
--- jma/utils/analysis.py
+++ jma/utils/analysis.py
@@ -1,5 +1,6 @@
 from jma.models import Earthquake
 from jma.models import EarthquakeDetail
+from django.db import transaction
 
 import xml.etree.ElementTree as ET
 import urllib.request
@@ -8,6 +9,10 @@ from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
+# 登録する震度の閾値
+regist_min_int = 3
+
+@transaction.atomic
 def analysis_xml(uuid):
     logger.info('*** exec utils.analysis.analysis_xml ***')
     uuid = '6c461de6-27fb-3ef6-b239-48f77c5607bc' #長野震度４
@@ -15,7 +20,6 @@ def analysis_xml(uuid):
     url = 'http://www.data.jma.go.jp/developer/xml/data/' + uuid + '.xml'
     logger.debug(url)
     req = urllib.request.Request(url)
-
 
     # XMLを読み込み
     with urllib.request.urlopen(req) as response:
@@ -26,17 +30,14 @@ def analysis_xml(uuid):
     ns = '{http://xml.kishou.go.jp/jmaxml1/body/seismology1/}'
 
     # 地震の情報を取得
-    # import pdb;pdb.set_trace()
-
-    # 地震の情報を取得
     bd = root.find(ns + 'Body')
-    # 発生日時 ->　Bodyタグ配下の子孫要素内で一番最初の'OriginTime'
+    # 発生日時 ->　'.//' をつけると子孫配下を探す
     datetime_str = bd.find('.//' +ns + 'OriginTime').text
     # 最大震度 ->　Bodyタグ配下の子孫要素内で一番最初の'MaxInt'
     max_int = bd.find('.//' + ns + 'MaxInt').text
-    # 震央 ->　Bodyタグ配下の子孫要素内で一番最初の'Name'
+    # 震源地 ->　Bodyタグ配下の子孫要素内で一番最初の'Name'
     area_name = bd.find('.//' + ns + 'Name').text
-    # 緯度経度はまだ
+        # 緯度経度はまだ
 
     # 登録
     eq = Earthquake(
@@ -47,13 +48,14 @@ def analysis_xml(uuid):
         upddt=datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     eq.save()
 
-    # City(市区町村)階層から市区町村コード、最大震度を取得
+    # City(市区町村)階層から市区町村コード、市区町村の最大震度を取得
     for city in root.iter(ns + 'City'):
         code = city.find(ns + 'Code').text
         city_maxint = city.find(ns + 'MaxInt').text
-        # 登録
-        eqd = EarthquakeDetail(
-            uuid=Earthquake.objects.get(id=eq.id),
-            intensity=city_maxint,
-            citycode=code)
-        eqd.save() # 一括登録（bulk_create）使ってみたい
+        # 震度が閾値以上の場合、登録
+        if int(city_maxint) >= regist_min_int:
+            eqd = EarthquakeDetail(
+                uuid=Earthquake.objects.get(id=eq.id), # 外部キーはEarthquakeオブジェクトから取らないと怒られる
+                intensity=city_maxint,
+                citycode=code)
+            eqd.save() # 一括登録（bulk_create）使ってみたい
